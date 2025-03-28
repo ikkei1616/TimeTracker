a78de69168217c67a0669217aa5c16b7c7174418
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-03-28 21:04:15 +0900

    mainの修正

---
+++ fuel/app/logs/2025/03/28.php
@@ -0,0 +1,4 @@
+<?php defined('COREPATH') or exit('No direct script access allowed'); ?>
+
+WARNING - 2025-03-28 21:03:42 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
+WARNING - 2025-03-28 21:03:48 --> Fuel\Core\Fuel::init - The configured locale en_US is not installed on your system.
