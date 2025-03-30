75a097a2714bb02997ce6cd3b888bcc0139e9ad0
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-03-30 19:44:40 +0900

    headerコンポーネントの作成

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return(
+    <>
+      <header className={"header"}>
+        <div className={"header-inner"}>
+          <div className={"header-logo"}>
+            <img src="/logo_TimeTracker.svg" alt="header-logo" />
+            <h1>Time Tracker</h1>
+          </div>
+        </div>
+      </header>
+    </>
+
+  );
+}
+export default Header;
