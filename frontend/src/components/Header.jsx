02281768d527eaaef40a8afe11386403711700ad
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-03-31 23:59:53 +0900

    headerコンポーネントの作成

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,11 +1,11 @@
 const Header = () => {
   return(
     <>
-      <header className={"header"}>
-        <div className={"header-inner"}>
-          <div className={"header-logo"}>
-            <img src="/logo_TimeTracker.svg" alt="header-logo" />
-            <h1>Time Tracker</h1>
+      <header className="w-screen header-after">
+        <div className="w-full max-w-7xl max-auto px-10 py-6">
+          <div className="flex items-center">
+            <img className="w-5 h-5 mr-2.5 " src="/logo_TimeTracker.svg" alt="header-logo" />
+            <h1 className="text-3xl font-bold">Time Tracker</h1>
           </div>
         </div>
       </header>
