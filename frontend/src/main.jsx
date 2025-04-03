98295e2ded598b8fad500b027d6488688cac11f5
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-04-03 19:59:23 +0900

    ルーティングの追加

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,27 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './pages/App'
+import SignUp from "./pages/SignUp"
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+  },
+  {
+    path:"/signUp",
+    element:<SignUp />,
+  }
+]);
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode >
-    <App/>
+    <RouterProvider router={router} />
   </StrictMode>,
 )
