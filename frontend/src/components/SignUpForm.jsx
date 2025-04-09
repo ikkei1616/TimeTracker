a5a8f33b6d0e2d8a8af9401e2b8f5da7a53ad244
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-04-09 21:49:10 +0900

    signUPフォームのデザイン

--- frontend/src/components/SignUpForm.jsx
+++ frontend/src/components/SignUpForm.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const SignUpForm = () => {
   return (
-    <div>signUpForm</div>
+    <form className="flex flex-col text-center  bg-yellow-300 w-111 p-12">
+      <div className="text-4xl font-bold">
+        <span className="gradationUnderBar  inline-block">SingUp</span>
+      </div>
+      <div className="mt-4 rounded-xl bg-gradient-to-r from-mainRed to-mainBlue p-0.5">
+        <input type="text"  className="w-full p-1.5 rounded-xl "/>
+      </div>
+      <div className="mt-4 rounded-xl bg-gradient-to-r from-mainRed to-mainBlue p-0.5">
+        <input type="text"  className="w-full p-1.5 rounded-xl "/>
+      </div>
+      <button type="submit" className="mt-4 rounded-xl p-2 bg-gradient-to-r from-mainRed to-mainBlue text-white font-bold">Register </button>
+    </form>   
   )
 }
 
