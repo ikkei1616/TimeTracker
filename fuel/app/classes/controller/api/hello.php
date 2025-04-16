424c61ee1a8074e772ab327504a3ac38be9419ae
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-04-16 12:39:58 +0900

    CORSヘッダーの変更

--- fuel/app/classes/controller/api/hello.php
+++ fuel/app/classes/controller/api/hello.php
@@ -6,10 +6,12 @@ class Controller_Api_Hello extends Controller_Rest
     protected $format = 'json';
 
     public function before()
-{
-    parent::before();
-    header('Access-Control-Allow-Origin: *'); // 本番では * は避けよう！
-}
+    {  
+        parent::before();
+        header('Access-Control-Allow-Origin: http://localhost:5173'); 
+        header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS'); 
+        header('Access-Control-Allow-Headers: Content-Type');
+    }
 
     public function get_index()
     {
