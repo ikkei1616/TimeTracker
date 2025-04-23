35efa5b68200f95a3120a5a338dafceb98f7e780
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-04-23 13:16:17 +0900

    インプットコンポーネント

---
+++ frontend/src/components/AuthInput.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+
+const AuthInput = ({value,setFunction,fieldName}) => {
+  let field
+  if (fieldName === "pass") {
+    field ="パスワード";
+  } else if (fieldName === "email") {
+    field = "メールアドレス";
+  }
+  else if (fieldName === "name") {
+    field = "名前";
+  }
+
+  return (
+    <div className="mt-4 rounded-xl bg-gradient-to-r from-mainRed to-mainBlue p-0.5">
+      <input
+        type="text"
+        placeholder={field}
+        className="w-full p-1.5 rounded-xl " 
+        value={value}
+        onChange={(e)=> setFunction(e.target.value)}                                                                        
+      >
+      </input>
+    </div>
+  )
+}
+
+export default AuthInput;
