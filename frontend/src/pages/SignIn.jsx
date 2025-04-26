432f9668e3682a594aee4644a31d9aa1fbf53698
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-04-26 14:46:27 +0900

    signIn画面を追加

---
+++ frontend/src/pages/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Title from "../components/Title"
+
+const  SignIn = () => {
+  return (
+    <div>
+      <Title/>
+      aaaa
+    </div>
+  )
+}
+
+export default SignIn;
