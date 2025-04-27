b3d82711557b862fa777a8928909d6d5dd798ce8
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-04-27 18:18:21 +0900

    エイリアスの変更

--- fuel/app/config/development/alias.php
+++ fuel/app/config/development/alias.php
@@ -7,5 +7,5 @@ class Cookie extends Fuel\Core\Cookie {}
 class Session extends Fuel\Core\Session {}
 class Controller_Rest extends Fuel\Core\Controller_Rest {}
 Class Log extends Fuel\Core\Log {}
-class Auth extends \Auth\Auth {}
+class Auth extends Auth\Auth_Login_Simpleauth {}
 class DB extends Fuel\Core\DB {}
