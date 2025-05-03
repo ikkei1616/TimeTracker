1fc8866ec9ff4fe965ab207620a7389a0bde272f
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-03 11:51:06 +0900

    TimerAreaが持つstateの追加

--- frontend/src/features/timerArea/TimerArea.jsx
+++ frontend/src/features/timerArea/TimerArea.jsx
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const TimerArea = () => {
+  const [time, setTime] = useState(null);
+  const [taskTitle, setTaskTitle] = useState(null);
+  const [isRunning, setIsRunning] = useState(false);
+  const [timeDiff, setTimeDiff] = useState(null);
+  const timerId = useRef(null);
   return (
     <div>TimerArea</div>
   )
