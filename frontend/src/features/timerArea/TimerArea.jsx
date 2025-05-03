94f90eb981aa69200d6159cc0be0666523ec8ff6
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-03 21:26:11 +0900

    数字を"00:00"の形式に変換する関数の実装

--- frontend/src/features/timerArea/TimerArea.jsx
+++ frontend/src/features/timerArea/TimerArea.jsx
@@ -10,7 +10,20 @@ const TimerArea = () => {
   const [timeDiff, setTimeDiff] = useState(null);
   const timerId = useRef(null);
   const [response,setResponse] = useState(null);
+ 
 
+  //数字を"00:00"の形式に変化する関数
+  function formatSecondsToTime(seconds) {
+    const mins = Math.floor(seconds / 60);
+    const secs = seconds % 60;
+    const paddedMins = String(mins).padStart(2, '0');
+    const paddedSecs = String(secs).padStart(2, '0');
+    return `${paddedMins}:${paddedSecs}`;
+  }
+
+  console.log(formatSecondsToTime(3600))
+  
+  
   useEffect(() => {
     if (isRunning) {
       timerId.current = setInterval(() => {
