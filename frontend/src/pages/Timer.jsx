62981a8953969de2c247b0b1cfc8d5e5b684e824
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-03 23:53:06 +0900

    メイン画面の雛形作成

--- frontend/src/pages/Timer.jsx
+++ frontend/src/pages/Timer.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React from 'react';
+import TimerArea from "../features/timerArea/TimerArea";
 
 const Timer = () => {
   return (
-    <div>Timer</div>
+    <div className="w-screen h-screen flex justify-center items-center">
+      <TimerArea/>
+    </div>
   )
 }
 
