add9ffcd5f9a136f07f0eded645db28015d0eedb
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-05 18:26:45 +0900

    startTask.jsの戻り値の追加

--- frontend/src/features/timerArea/api/startTask.js
+++ frontend/src/features/timerArea/api/startTask.js
@@ -14,7 +14,14 @@ export const startTask = async ({taskTitle,setResponse}) => {
     });
     
     const data = await res.json();
+    console.log("取得データ",data)
     setResponse(data.message)
+    const responseMessage = data.message;
+    const receivedTaskTitle = data.tasks.title; 
+    const serverTime = data.server_time;
+
+    return {responseMessage,receivedTaskTitle,serverTime}
+
   } catch (error) {
     console.log("Error: ",error);
     setResponse("タスクの開始失敗しました。")
