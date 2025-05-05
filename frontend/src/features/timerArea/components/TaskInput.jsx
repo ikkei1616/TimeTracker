a282bc319f455cb2e244bc6525326cb9e3d1ec15
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-05 20:00:35 +0900

    デザイン修正

--- frontend/src/features/timerArea/components/TaskInput.jsx
+++ frontend/src/features/timerArea/components/TaskInput.jsx
@@ -6,7 +6,7 @@ const TaskInput = ({taskTitle,setTaskTitle}) => {
       value={taskTitle}
       type="text"
       placeholder="タスク名を入力してください"
-      className="w-96 h-16 p-4 text-color-ccc text-2xl font-bold"
+      className="w-96 h-16 p-4 pl-1 text-color-ccc text-2xl font-bold"
       onChange={(e) => setTaskTitle(e.target.value)}
     />
   )
