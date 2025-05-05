3d4e904a022d7501b8cb5a32aea7efed69cea0eb
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-05 19:25:14 +0900

    TaskTitleコンポーネントの切り出し&デザイン実装

---
+++ frontend/src/features/timerArea/components/TaskTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskTitle= ({taskTitle}) => {
+  return (
+    <div
+      className="pl-20 text-4xl text-center font-bold"
+    >{taskTitle}</div>
+  )
+}
+
+export default TaskTitle;
