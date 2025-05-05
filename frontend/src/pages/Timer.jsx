8a7ba59c0afcc1a46ac84cf3140962801fa50b24
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-05 20:01:17 +0900

    スタイルの変更

--- frontend/src/pages/Timer.jsx
+++ frontend/src/pages/Timer.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import TimerArea from "../features/timerArea/TimerArea";
+import Header from "../components/Header";
 
 const Timer = () => {
   return (
+    <div className="w-screen  flex flex-col justify-center items-center">
       <Header/>
       <TimerArea/>
     </div>
