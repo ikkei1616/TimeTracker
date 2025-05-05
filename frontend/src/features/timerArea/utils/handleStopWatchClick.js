61422fed698ad35af43e835b7462853ed2c1c00b
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-05 18:38:49 +0900

    ストップウォッチが押された時の関数切り出し

---
+++ frontend/src/features/timerArea/utils/handleStopWatchClick.js
@@ -0,0 +1,30 @@
+
+import {startTask} from "../api/startTask"
+import {endTask} from "../api/endTask";
+
+export  const handleStopWatchClick = ({isRunning,setIsRunning,setTaskTitle,setTime,setResponse,timerId,taskTitle,setTimeDiff,taskStartTime}) => {
+    console.log("クリックされました");
+    if (isRunning ){
+      //タイマーストップ時の処理
+      setTaskTitle("");
+      setTime(null);
+      endTask({setResponse});
+      setIsRunning((state) => !state)
+      clearInterval(timerId.current)
+    } else {
+      //タイマー開始時の処理
+      const startTaskHandler = async () => {
+        const {responseMessage,receivedTaskTitle,serverTime} = await startTask({taskTitle,setResponse})
+        const clientTime = new Date(); 
+        console.log("clientTime",clientTime);
+        console.log("timeDiff",clientTime - new Date(serverTime))
+        
+        setResponse(responseMessage);
+        setTaskTitle(receivedTaskTitle);
+        setTimeDiff(clientTime - new Date(serverTime));
+        taskStartTime.current = new Date(serverTime);
+        setIsRunning(prev => !prev)
+      }
+      startTaskHandler();
+    }
+  }
