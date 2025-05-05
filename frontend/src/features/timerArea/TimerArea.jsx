3dc859615b4d2dad1ac922d4b1c532e70a0a6763
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-05 18:40:04 +0900

    hooks,関数の切り出し

--- frontend/src/features/timerArea/TimerArea.jsx
+++ frontend/src/features/timerArea/TimerArea.jsx
@@ -1,18 +1,26 @@
 import React from "react";
 import { useState, useEffect, useRef } from "react";
-import {startTask} from "./api/startTask";
-import {endTask} from "./api/endTask";
+import  useTimer  from "./hooks/useTimer";
+import { getCurrentTask } from "./api/getCurrentTask";
+import { formatSecondsToTime } from "./utils/formatSecondsToTIme";
+import { handleStopWatchClick } from "./utils/handleStopWatchClick";
+
 
 const TimerArea = () => {
   const [time, setTime] = useState(null);
   const [taskTitle, setTaskTitle] = useState("");
   const [isRunning, setIsRunning] = useState(false);
   const [timeDiff, setTimeDiff] = useState(null);
-  const timerId = useRef(null);
   const [response,setResponse] = useState(null);
+  const timerId = useRef(null);
+  const taskStartTime = useRef(null);
+
   useEffect(()=>{
     getCurrentTask({setIsRunning,setResponse,setTaskTitle,setTimeDiff,taskStartTime});
   },[])
+  
+  useTimer({timerId,taskStartTime,timeDiff,setTime,isRunning})
+
 
   return (
     <div className="flex gap-16">
@@ -27,11 +35,11 @@ const TimerArea = () => {
           onChange={(e) => setTaskTitle(e.target.value)}
         />
       )}
-
+      
       <div className="flex gap-4">
         <div>{formatSecondsToTime(time)}</div>
         <button
-          onClick={handleClick}
+          onClick={()=>{handleStopWatchClick({isRunning,setIsRunning,setTaskTitle,setTime,setResponse,timerId,taskTitle,setTimeDiff,taskStartTime})}}
           className="border border-black border-solid"
           disabled={!taskTitle}
         >
