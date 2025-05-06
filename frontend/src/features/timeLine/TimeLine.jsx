128c8fa28579e6e46eb95fb3f619bf8c3828fb9e
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-06 14:59:30 +0900

    タスクの取得、タスクの表示位置計算ロジック、タスクの縦幅計算ロジックの実装

--- frontend/src/features/timeLine/TimeLine.jsx
+++ frontend/src/features/timeLine/TimeLine.jsx
@@ -1,6 +1,63 @@
 import React from 'react'
+import { useState,useEffect } from 'react';
 
 const TimeLine = () => {
+  const [tasks,setTasks ] = useState([]);
+
+  useEffect(()=>{
+    const getTasks = async () => {
+      try {
+        const res = await fetch("http://localhost/api/task/tasks",{
+          Post:"GET",
+          credentials:"include",
+        });
+
+        if (res.ok) {
+          const data = await res.json();
+          console.log("取得したdeta",data)
+
+        
+          
+          const addedTasks = data.tasks.map((task)=> {
+
+            const taskStartTime = task.start_time;
+            if (!data.end_time) {
+              let taskEndTime = new Date();
+            }
+            let taskEndTime = task.end_time;
+
+            //タスクの表示位置計算のロジック
+            const timeStandard = new Date();
+            timeStandard.setHours(0);
+            timeStandard.setMinutes(0);
+            timeStandard.setSeconds(0);
+            timeStandard.setMilliseconds(0);
+            const durationMs =(new Date(taskStartTime) - timeStandard)
+            const durationHour = ( durationMs / 3600000);
+            task.topLength = (durationHour + 9 );
+
+            //タスクの長さ計算のロジック
+            const elapsedMs = new Date(taskEndTime) - new Date(taskStartTime);
+            const elapsedHour = elapsedMs / 3600000;
+            console.log("タスク開示時間",taskStartTime);
+            console.log("タスク終了時間",taskEndTime)
+            console.log("タスク所要時間",elapsedHour);
+            task.height = elapsedHour;
+
+            return task
+          })
+
+          
+          setTasks(addedTasks);
+        }
+        
+      } catch (error) {
+        console.log("error",error);
+      }
+    }
+    getTasks();
+  },[])
+
   return (
     <div className="w-9/12 ">
       <h2 className="text-left border-2">Task Time Line</h2>
@@ -19,12 +76,15 @@ const TimeLine = () => {
                   return (<div key={i} className="h-[60px] border border-black">{i}</div>)
                 })}
               </div>
+              {tasks.map((task)=>{
+                console.log("タスクmap関数",task)
+                return (<div style={{ position:"absolute", top:`${task.topLength * 60}px`, backgroundColor:"blue",color:"white",height:`${task.height * 60}px` ,width:"90%" ,marginLeft:"5%" }}>{task.title}</div>)
 
               })}
           </div>
         </div>
       </div>
-              </div>
+    </div>
   )
 }
 
