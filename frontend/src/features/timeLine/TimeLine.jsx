2918f26417ce24e93ab65aa0a0eb4221c830cefd
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-06 14:58:17 +0900

    timeLineの枠組みの雛形実装

--- frontend/src/features/timeLine/TimeLine.jsx
+++ frontend/src/features/timeLine/TimeLine.jsx
@@ -2,7 +2,29 @@ import React from 'react'
 
 const TimeLine = () => {
   return (
-    <div>TimeLine</div>
+    <div className="w-9/12 ">
+      <h2 className="text-left border-2">Task Time Line</h2>
+      <div>
+        <div className="text-center border-2">4/1 Monday</div>
+        <div className="h-[1200px]  flex justify-between mb-60 ">
+          <div className='relative w-2/12 border-2 border-rose-300 h-[1440px]'>
+            {Array.from({length:23}).map((_, i) => {
+              return <p key={i} style={{ position:"absolute",top: `${(i+1) * 60 -10}px`,right:20}} >{i+1}:00</p>
+              
+            })}
+          </div>
+          <div className='relative grid  grid-rows-24  w-10/12 bg-white '>
+              <div>
+                {Array.from({length:24}).map((_, i) => {
+                  return (<div key={i} className="h-[60px] border border-black">{i}</div>)
+                })}
+              </div>
+
+              })}
+          </div>
+        </div>
+      </div>
+              </div>
   )
 }
 
