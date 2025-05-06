44b4bfa02f60883d73ab7c9a6e3f314ddb956e31
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-06 14:47:36 +0900

    TimeLineコンポーネントの導入

--- frontend/src/pages/Timer.jsx
+++ frontend/src/pages/Timer.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import TimerArea from "../features/timerArea/TimerArea";
 import Header from "../components/Header";
+import TimeLine from '../features/timeLine/TimeLine';
 
 const Timer = () => {
   return (
     <div className="w-screen  flex flex-col justify-center items-center">
       <Header/>
       <TimerArea/>
+      <TimeLine/>
     </div>
   )
 }
