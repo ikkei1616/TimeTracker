c5466b27849fb49507f7156a186b01cb5bdfd8e1
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-05-09 21:14:38 +0900

    setIsSignInをApp内に留める

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header";
-import {useState,useEffect} from "react";
+import {useState,useLayoutEffect} from "react";
 import {signInCheck} from "../features/auth/api";
 import SignIn from "./signIn";
 import SignUp from "./SignUp"
@@ -8,6 +8,8 @@ function App() {
   const [isSignIn, setIsSignIn] =useState(null);
 
   useLayoutEffect(()=>{
+    const isSignInResult = signInCheck();
+    setIsSignIn(isSignInResult);
   },[])
   return (
     <>
