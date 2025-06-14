df2a11c03110955224b91c43209228372234b5b4
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-06-14 18:38:11 +0900

    useTasksの実装

---
+++ frontend/src/features/timeLine/hooks/useTasks.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState, useMemo } from "react";
+import { getTasks } from "../api/getTasks";
+import { addTaskProperties } from "../utlis/addTaskProperties";
+import { filterDisplayedTask } from "../utlis/filterDisplayedTask";
+
+export const useTasks = () => {
+  const [tasks, setTasks] = useState([]);
+  const [displayedTasks, setDisplayedTasks] = useState([]);
+  const today = useMemo(() => new Date(), []);
+
+  const changeDisplayedTasks = ({ type, displayedDate }) => {
+    let dateFilter = new Date(displayedDate);
+    switch (type) {
+      case "tomorrow": {
+        const nextDay = displayedDate.getDate() + 1;
+        dateFilter.setDate(nextDay);
+        break;
+      }
+      case "yesterday": {
+        const previousDay = displayedDate.getDate() - 1;
+        dateFilter.setDate(previousDay);
+        break;
+      }
+    }
+    const filteredDisplayedTasks = tasks.filter((task) => {
+      const isFilter = task.getDate() === dateFilter.getDate();
+      return isFilter;
+    });
+    setDisplayedTasks(filteredDisplayedTasks);
+  };
+
+
+
+  useEffect(() => {
+    
+    getTasks()
+      .then((tasks) => addTaskProperties(tasks))
+      .then((processedTasks) => {
+        setTasks(processedTasks);
+        return processedTasks;
+      })
+      .then((processedTasks) => {
+        const filteredDisplayedTask = filterDisplayedTask(processedTasks);
+        setDisplayedTasks(filteredDisplayedTask);
+      })
+      .catch((e) => {
+        throw new Error(e);
+      });
+  }, [today]);
+
+  return { displayedTasks, setTasks, setDisplayedTasks, changeDisplayedTasks};
+};
