5e841d3a037b331c4c626f94fb4b8697c2eb900a
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-06-14 19:06:54 +0900

    taskDelete関数の実装

--- frontend/src/features/timeLine/api/taskDelete.js
+++ frontend/src/features/timeLine/api/taskDelete.js
@@ -1,15 +1,19 @@
 import { apiFetcher } from "../../../utils/apiFetcher";
 
-export const taskDelete = async ({taskId,setTasks,setResponse}) => {
+export const taskDelete = async ({ taskId, setTasks }) => {
+  const data = await apiFetcher({
+    httpMethod: "DELETE",
+    pass: `http://localhost/api/task/tasks/${taskId}`,
+  }); 
+  setTasks((prev) => prev.filter((task) => task.id !== data.tasks[0].id));
+  return data;
+};
 
-  try {
-    const data = await apiFetcher({httpMethod:"DELETE",pass:`http://localhost/api/task/tasks/${taskId}`})
 
-    setTasks((prev)=> prev.filter(task => task.id !== data.tasks[0].id));
+//楽観的な更新になっている。DBの状態とUIが同期していない
+//optimisticなUIと言われている。
+//バックエンドがDBから削除したことを受けて、その後にフロントから削除したい
+//今回の場合だと、DBからタスクが削除されていない状態でも、UIから削除される可能性がる。
 
-  } catch (error) {
-    console.error("Error:task delete",error);
-    setResponse(error)
-  }
 
-}
+//taskDeleteだと、タスクの削除だけを担当しているように見えるし、そうしたい。
