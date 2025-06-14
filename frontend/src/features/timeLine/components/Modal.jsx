5f02f597ef3a1a6e734d83de84a4a1bd5d25cbd1
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-06-14 19:14:14 +0900

    リファクタ

--- frontend/src/features/timeLine/components/Modal.jsx
+++ frontend/src/features/timeLine/components/Modal.jsx
@@ -1,40 +1,38 @@
-import React from "react";
 import { useState } from "react";
 import { taskDelete } from "../api/taskDelete";
 import { taskEdit } from "../api/taskEdit";
-import {clsx} from "clsx";
+import { clsx } from "clsx";
 
 const Modal = ({ task, setIsModalOpen, setTasks }) => {
   const [response, setResponse] = useState(null);
   const [reName, setReName] = useState("");
-  
-
 
   const taskId = task.id;
- 
- 
 
   const handleDelete = () => {
-    console.log("デリートボタンがクリックされました。");
-    taskDelete({ taskId, setTasks, setResponse });
-    setIsModalOpen(false);
+    taskDelete({ taskId, setTasks })
+      .then((data) => {
+        setTasks((prev) => prev.filter((task) => task.id !== data.tasks[0].id));
+        setIsModalOpen(false);
+      })
+      .catch((error) => setResponse(error));
   };
 
   const handleEdit = () => {
-    console.log("クリックはできてるよ")
-    const {resStatus,editedTask} = taskEdit({taskId,title:reName});
-    console.log(resStatus);
-    setTasks(editedTask);
+    taskEdit({ taskId, title: reName }).then((resStatus, editedTask) => {
+      setTasks(editedTask);
+      console.log(resStatus);
+    });
   };
 
   return (
     <div
       className="absolute w-96 h-28 p-4 border-2 z-20 left-[50%] text-mainBlack  border-mainBlack bg-white rounded-lg shadow-2xl font-bold"
-      style={{ top: `${task.fromTopDistance * 60 + task.height * 20}px` }}
+      style={{ top: `${task.elapsedHourSinceMidnight * 60 + task.elapsedHour * 20}px` }}
     >
       <div className="flex justify-between mb-2">
         <p className="text-lg">{task.title}</p>
-        <p 
+        <p
           onClick={(e) => {
             e.stopPropagation();
             setIsModalOpen(false);
@@ -55,18 +53,16 @@ const Modal = ({ task, setIsModalOpen, setTasks }) => {
             e.stopPropagation();
             handleEdit();
           }}
-          className={
-            clsx("w-20   text-white rounded-lg",
-              reName ? "bg-gradient-to-r from-mainRed to-mainBlue": "bg-mainGray"
-            )
-          }
+          className={clsx(
+            "w-20   text-white rounded-lg",
+            reName ? "bg-gradient-to-r from-mainRed to-mainBlue" : "bg-mainGray"
+          )}
           disabled={!reName}
-          
         >
           edit
         </button>
-        <button 
-          onClick={handleDelete} 
+        <button
+          onClick={handleDelete}
           className="w-20  bg-gradient-to-r bg-mainBlack text-white rounded-lg"
         >
           delete
