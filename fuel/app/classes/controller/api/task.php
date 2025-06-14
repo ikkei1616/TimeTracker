7445cba278e56d665dbe438eb358fdfdd5202eeb
ikkei1616 <is072xe@ed.ritsumei.ac.jp>
2025-06-14 19:02:55 +0900

    CORSの設定変更

--- fuel/app/classes/controller/api/task.php
+++ fuel/app/classes/controller/api/task.php
@@ -11,7 +11,7 @@ class Controller_Api_Task extends Controller_Rest
   public function before()
   {
     parent::before();
-    header('Access-Control-Allow-Origin: http://localhost:5173');
+    header('Access-Control-Allow-Origin: http://localhost:5174');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE,PATCH,OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type,X-CSRF-Token');
     header('Access-Control-Allow-Credentials: true');
